abcb5ab3c17c34b42fdf7471baed41abb22732a4
Gianny Rivera <giedriveraza@ittepic.edu.mx>
2017-09-11 14:20:18 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var converter = require('number-to-words');
+
+ejem1=converter.toOrdinal(21);
+ejem2=converter.toWords(34);
+ejem3=converter.toWordsOrdinal(44);
+
+console.log(ejem1);
+console.log(ejem2);
+console.log(ejem3)
